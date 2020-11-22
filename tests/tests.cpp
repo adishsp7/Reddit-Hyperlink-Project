16801d5a4072c45f6d1e8cfdd541f082b09a5da2
Adish <adish2@illinois.edu>
2020-11-22 11:21:58 -0600

    Basic Makefile, Tests and cs225 folder structure

---
+++ tests/tests.cpp
@@ -0,0 +1,12 @@
+#include "../cs225/catch/catch.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stack>
+
+
+using namespace cs225;
+using namespace std;
+
+

a45e295310467e6e9030954cd30e18d9ec35f83e
Adish <adish2@illinois.edu>
2020-11-23 23:52:02 -0600

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "reddit.h"
+#include <vector>
+#include "edge.h"
+
+
+int main() {
+    
+/*
+  // Print textual output of the graph:
+  nim.getGraph().print();
+
+  // Save an graph PNG:
+  nim.getGraph().savePNG("Out");
+*/
+
+  return 0;
+}

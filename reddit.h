9d164b0e038eb0541e39027cf740d2690c5bcc7e
Mike Lee <dcl3@illinois.edu>
2020-12-03 00:06:34 -0600

    Update reddi.h

--- reddit.h
+++ reddit.h
@@ -29,33 +29,19 @@ class Reddit{
 
     vector<Vertex> BFS(Vertex src, Vertex goal);
 
-<<<<<<< HEAD
     Reddit();
     void BFS(Graph & g_);
     void BFSHelper(Graph & g_, Vertex v);
     bool Reddit::IDS(string node, string goal, int depth);
     void StronglyCCUtil(Vertex u, unordered_map<Vertex, int> disc, unordered_map<Vertex, int> low, stack<Vertex> *st, unordered_map<Vertex, bool> stackMember);
     void StronglyCC();
-=======
-    vector<vector<Vertex>> SCCs();
-    void DFS(Vertex src, Graph & g, unordered_set<Vertex> & visited, stack<Vertex> & s);
-
-    vector<Vertex> IDS(string src, string goal, int depth);
-    bool IDSutil(unordered_set<Vertex> & visited, vector<Vertex> & path, Vertex node, Vertex goal, int depth);
-    int maxDepth(Vertex src);
-    int dUtil(unordered_map<Vertex, int> & depth, Vertex node, Vertex src);
->>>>>>> 64296e845f879031bb79d02007ed5a19aff9d5e0
 
     private:
     // Graph redditg_;
     // Vertex startvertex_;
     Graph g_;
-<<<<<<< HEAD
     vector<vector<Vertex>> result;
 
     
-=======
-    Graph gT_;
->>>>>>> 64296e845f879031bb79d02007ed5a19aff9d5e0
 
 };   

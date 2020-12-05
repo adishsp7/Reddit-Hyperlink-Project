ad59eb5d05bccd9e55a60ea3ad5395ec5d5d433b
Kim, John <jaehank2@illinois.edu>
2020-12-06 01:19:29 +0900

    added DFS to public and renamed private DFS to SCCUtil

--- reddit.h
+++ reddit.h
@@ -25,6 +25,7 @@ class Reddit{
     vector<Vertex> IDS(string src, string goal, int depth);
     vector<vector<Vertex>> SCCs();
     vector<vector<Vertex>> StronglyCC();
+    void DFS(Vertex src, Graph & g, unordered_set<Vertex> & visited, stack<Vertex> & s);
     // void DFS(Vertex src, const Graph & g, unordered_set<Vertex> & visited, stack<Vertex> & s);
     const Graph & getGraph() const;
     const Graph & getTranspose() const;
@@ -33,7 +34,7 @@ class Reddit{
 
     private:
     void StronglyCCUtil(Vertex u, unordered_map<Vertex, int> & disc, unordered_map<Vertex, int> & low, stack<Vertex> & st, unordered_map<Vertex, bool> & stackMember);
-    void DFS(Vertex src, Graph & g, unordered_set<Vertex> & visited, stack<Vertex> & s);
+    void SCCUtil(Vertex src, Graph & g, unordered_set<Vertex> & visited, stack<Vertex> & s);
     bool IDSutil(unordered_set<Vertex> & visited, vector<Vertex> & path, Vertex node, Vertex goal, int depth);
     
     Graph g_;

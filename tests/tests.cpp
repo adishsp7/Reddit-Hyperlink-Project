4e1ee5777584af0f4c5c3b2e04bf9435661de8b6
yashg3 <yashg3@illinois.edu>
2020-12-06 05:58:59 -0600

    Added tests for IDS

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,6 +10,56 @@
 
 //using namespace cs225;
 using namespace std;
+int maxDepth(Reddit & red, Vertex src);
+int dUtil(const Graph & g, unordered_map<Vertex, int> & depth, Vertex node);
+
+/**
+ * Returns Max depth of vertex in reddit graph
+ * @param red - Reddit graph object
+ * @param src - Vertex for which max depth is required
+ * @return Max depth of src vertex
+ */
+int maxDepth(Reddit & red, Vertex src)
+{
+    const Graph & g = red.getGraph();
+    if(g.vertexExists(src))
+    {
+      unordered_map<Vertex, int> depth; // initializes depth map
+      return dUtil(g, depth, src); // starts recursion for src node
+    }
+    cout << "Vertex doesn't exist!" << endl;
+    return -1;
+}
+
+/**
+ * Calls dUtil to find max depth
+ * @param depth - Unordered map that tracks max depth of each node
+ * @param node - Current recursive call's vertex
+ * @return Max depth of current node using DFS
+ */
+int dUtil(const Graph & g, unordered_map<Vertex, int> & depth, Vertex node)
+{
+  int max = 0; // initializes max depth tracker for current vertex to 0
+  depth[node] = -1;
+  vector<Vertex> vertex_list = g.getAdjacent(node);  // get a list of adjacent vertices
+  for (size_t i = 0; i < vertex_list.size(); i++) // iterates over adjecent vertices
+  {
+    int curr; // initializes max depth tracker for this adjecenct vertex
+    if(depth.find(vertex_list[i]) == depth.end()) // checks if this adjecent vertex is unvisted
+    {
+      curr = dUtil(g, depth, vertex_list[i]) + 1; // calls dUtil to get max depth for this adjecent vertex
+    }
+    else // this vertex has already been visited
+    {
+      curr = depth[vertex_list[i]] + 1; // gets max depth of this adjecent vertex
+    }
+    max = (curr > max) ? curr : max; // sets max to the maximum of current adjecent vertex depth and previously iterated adjecent vertices
+  }
+  depth[node] = max; // adds current vertex to depth map and assigns max as its depth
+  return max; // returns max depth of current vertex
+}
+
+
 
 TEST_CASE("Reddit(file) constructor checks if empty", "[weight=1][part=1][constructor]") {
   Reddit red("tests/empty.csv");
@@ -145,12 +195,11 @@ TEST_CASE("Reddit(file) Constructor Test - Large #2", "[weight=1][part=1][constr
   } 
 }
 
-
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
 TEST_CASE("BFS creates correct shortest path traversal - Small", "[weight=1][part=1][BFS]"){
   Reddit red("tests/small.csv"); //constructs reddit object using constructor
-  const Graph & g = red.getGraph(); //get graph from reddit object
 
-  vector<Vertex> test1to5 = {"1", "3", "2", "4", "5"}; //test for BFS shortest path from vertices 1 to 5
+  vector<Vertex> test1to5 = {"5", "4", "2", "3", "1"}; //test for BFS shortest path from vertices 1 to 5 in reverse
   vector<Vertex> sol1to5 = red.BFS("1", "5"); //solution from BFS function
 
   vector<Vertex> test3to1 = {}; //test for BFS shortest path from vertices 3 to 1 (NOT POSSIBLE);
@@ -161,41 +210,40 @@ TEST_CASE("BFS creates correct shortest path traversal - Small", "[weight=1][par
 
 }
 
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
 TEST_CASE("BFS creates correct shortest path traversal - Medium", "[weight=1][part=1][BFS]"){
   Reddit red("tests/medium.csv"); //constructs reddit object using constructor
-  const Graph & g = red.getGraph(); //get graph from reddit object
 
-  vector<Vertex> testDtoA = {"D", "H", "G", "E", "K", "A"}; //test for BFS shortest path from vertices 1 to 5
+  vector<Vertex> testDtoA = {"A", "K", "E", "G", "H", "D"}; //test for BFS shortest path from vertices D to A in reverse
   vector<Vertex> solDtoA = red.BFS("D", "A"); //solution from BFS function
 
   REQUIRE(testDtoA == solDtoA);
 
 }
 
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
 TEST_CASE("BFS creates correct shortest path traversal - Large #1", "[weight=1][part=1][BFS]"){
   Reddit red("tests/large1.csv"); //constructs reddit object using constructor
-  const Graph & g = red.getGraph(); //get graph from reddit object
 
-  vector<Vertex> testAtoB = {"A", "D", "F", "C", "B"}; //test for BFS shortest path from vertices 1 to 5
+  vector<Vertex> testAtoB = {"B", "C", "F", "D", "A"}; //test for BFS shortest path from vertices A to B in reverse
   vector<Vertex> solAtoB = red.BFS("A", "B"); //solution from BFS function
 
   REQUIRE(testAtoB == solAtoB);
 
 }
 
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
 TEST_CASE("BFS creates correct shortest path traversal - Large #2", "[weight=1][part=1][BFS]"){
   Reddit red("tests/large2.csv"); //constructs reddit object using constructor
-  const Graph & g = red.getGraph(); //get graph from reddit object
 
-  vector<Vertex> testAto2 = {"A", "1", "3", "2"}; //test for BFS shortest path from vertices 1 to 5
+  vector<Vertex> testAto2 = {"2", "3", "1", "A"}; //test for BFS shortest path from vertices A to 2 in reverse
   vector<Vertex> solAto2 = red.BFS("A", "2"); //solution from BFS function
 
   REQUIRE(testAto2 == solAto2);
 
 }
 
-
-TEST_CASE("IDS creates correct traversal ", "[weight=1][part=1]"){
+TEST_CASE("IDS creates correct traversal ", "[weight=1][part=1][IDS]"){
   Reddit red("tests/empty.csv");   // test case for 5 vertices
 
   const Graph & g = red.getGraph();
@@ -206,6 +254,51 @@ TEST_CASE("IDS creates correct traversal ", "[weight=1][part=1]"){
   // checking for valid path in progress - will update soon
 
 }
+
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
+TEST_CASE("IDS creates the same path as BFS - Small", "[weight=1][part=1][IDS]"){
+  Reddit red("tests/small.csv"); //constructs reddit object using constructor
+  
+  int maxD = maxDepth(red, "1"); //gets maximum explorable depth from test source
+  vector<Vertex> expected = red.BFS("1", "5"); //solution from BFS function
+  vector<Vertex> actual = red.IDS("1", "5", maxD); //solution from IDS function
+
+  REQUIRE(expected == actual);
+}
+
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
+TEST_CASE("IDS creates the same path as BFS - Medium", "[weight=1][part=1][IDS]"){
+  Reddit red("tests/medium.csv"); //constructs reddit object using constructor
+
+  int maxD = maxDepth(red, "D"); //gets maximum explorable depth from test source
+  vector<Vertex> expected = red.BFS("D", "A"); //solution from BFS function
+  vector<Vertex> actual = red.IDS("D", "A", maxD); //solution from IDS function
+
+  REQUIRE(expected == actual);
+}
+
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
+TEST_CASE("IDS creates the same path as BFS - Large #1", "[weight=1][part=1][IDS]"){
+  Reddit red("tests/large1.csv"); //constructs reddit object using constructor
+
+  int maxD = maxDepth(red, "A"); //gets maximum explorable depth from test source
+  vector<Vertex> expected = red.BFS("A", "B"); //solution from BFS function
+  vector<Vertex> actual = red.IDS("A", "B", maxD); //solution from IDS function
+
+  REQUIRE(expected == actual);
+}
+
+// Traversal test cases expect path in reverse order destination <- source due to function optimization
+TEST_CASE("IDS creates the same path as BFS - Large #2", "[weight=1][part=1][IDS]"){
+  Reddit red("tests/large2.csv"); //constructs reddit object using constructor
+
+  int maxD = maxDepth(red, "A"); //gets maximum explorable depth from test source
+  vector<Vertex> expected = red.BFS("A", "2"); //solution from BFS function
+  vector<Vertex> actual = red.IDS("A", "2", maxD); //solution from IDS function
+
+  REQUIRE(expected == actual);
+}
+
 
 TEST_CASE("DFS creates correct traversal ", "[weight=1][part=1]"){
   Reddit red("tests/small.csv");   // test case for 5 vertices

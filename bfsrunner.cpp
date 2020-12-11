06db020f141088951d61acb9e4372b8448f6c701
yashg3 <yashg3@illinois.edu>
2020-12-11 11:43:19 -0600

    Added comments

--- bfsrunner.cpp
+++ bfsrunner.cpp
@@ -1,15 +1,15 @@
 #include "reddit.h"
-#include <vector>
 
 int main(int argc, char** argv) 
 {
-    if(argc != 4)
+    if(argc != 4) // Ensures correct number of arguments
     {
-        std::cout << "Function requires arguments: [Filepath to data set] [Source Subreddit] [Target Subreddit]" << std::endl; 
+        std::cout << "Function requires arguments: [Filepath to data set] [Source Subreddit] [Target Subreddit]" << std::endl; // Prints expected arguments 
+        return 0; // Returns
     }
-    Reddit red(argv[1]);
-    vector<Vertex> path1 = red.BFS(argv[2], argv[3]);
-    std::cout << "BFS path: "; 
-    red.printPath(path1);
-    std::cout << "Path sentiment: " << red.getSentiment(path1) << std::endl;
+    Reddit red(argv[1]); // Creates reddit object using dataset file path
+    vector<Vertex> path1 = red.BFS(argv[2], argv[3]); //BFSs from source subreddit to target subreddit
+    std::cout << "BFS path: ";
+    red.printPath(path1); // Prints path
+    std::cout << "Path sentiment: " << red.getSentiment(path1) << std::endl; // Prints path sentiment
 }

4c29475440bfff65b71af94d10910dea0ddb7ec2
yashg3 <yashg3@illinois.edu>
2020-12-11 11:26:39 -0600

    Added comments

--- sccrunner.cpp
+++ sccrunner.cpp
@@ -1,48 +1,46 @@
 #include "reddit.h"
-#include <vector>
-#include <string>
-#include <fstream>
 
 int main(int argc, char** argv) 
 {
-    if(argc != 2)
+    if(argc != 2) // Ensures corrent number of arguments
     {
-        std::cout << "Function requires arguments: [Filepath to data set]" << std::endl; 
+        std::cout << "Function requires arguments: [Filepath to data set]" << std::endl; // Prints expected arguments
+        return 0; // Returns
     }
-    Reddit red(argv[1]);
-    system("ulimit -s unlimited");
-    std::ofstream f1;
+    Reddit red(argv[1]); // Creates reddit object using dataset file path
+    system("ulimit -s unlimited"); // Set stack size to unlimited
+    std::ofstream f; // Creates file object
+    int counter; // Internal counter for pretty printing
 
-    vector<vector<Vertex>> kosaraju = red.SCCs();
+    vector<vector<Vertex>> kosaraju = red.SCCs(); // Stores kosarajus SCCs
     std::cout << "Writing SCCs found using Kosaraju's algorithm to 'kosarajus.txt'" << std::endl;
-    f1.open ("kosarajus.txt");
-    int counter;
-    for(auto & vec : kosaraju)
+    f.open ("kosarajus.txt"); // Creates text file call kosarajus.txt to store output
+    for(auto & vec : kosaraju) // Iterates over each SCC
     {
-        counter = 0;
-        for(auto scc: vec)
+        counter = 0; // Sets counter to 0
+        for(auto scc: vec) // Iterates over vertices in this SCC
         {
-            f1 << scc << " ";
-            counter++;
-            if(counter % 9 == 0) f1 << "\n";
+            f << scc << " "; // Writes vertex to text file
+            counter++; // increments counter
+            if(counter % 9 == 0) f << "\n"; // ensures only 10 vertices are printed per line
         }
-        f1 << "\n***\n";
+        f << "\n***\n"; // Aesthetic purposes
     }
-    f1.close();
+    f.close(); // closes text file
 
-    vector<vector<Vertex>> tarjans = red.StronglyCC();
+    vector<vector<Vertex>> tarjans = red.StronglyCC(); // Stores tarjans SCCs
     std::cout << "Writing SCCs found using Tarjan's algorithm to 'tarjans.txt'" << std::endl;
-    f1.open ("tarjans.txt");
-    for(auto & vec : tarjans)
+    f.open ("tarjans.txt"); // Creates text file call tarjans.txt to store output
+    for(auto & vec : tarjans) // Iterates over each SCC
     {
-        counter = 0;
-        for(auto scc: vec)
+        counter = 0; // Sets counter to 0
+        for(auto scc: vec) // Iterates over vertices in this SCC
         {
-            f1 << scc << " ";
-            counter++;
-            if(counter % 9 == 0) f1 << "\n";
+            f << scc << " "; // Writes vertex to text file
+            counter++; // increments counter
+            if(counter % 9 == 0) f << "\n"; // ensures only 10 vertices are printed per line
         }
-        f1 << "\n***\n";
+        f << "\n***\n"; // Aesthetic purposes
     }
-    f1.close();   
+    f.close(); // closes text file
 }

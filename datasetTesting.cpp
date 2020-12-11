e8ce845686c3c3228005bbd9fe1965c0fdb91a8f
yashg3 <yashg3@illinois.edu>
2020-12-11 11:15:55 -0600

    Added sample function calls to all class functions for reference

--- datasetTesting.cpp
+++ datasetTesting.cpp
@@ -1,117 +1,41 @@
 #include "reddit.h"
-#include <vector>
-#include "edge.h"
-#include <chrono>
-#include <algorithm>
 
 
-void sccSpeed(Reddit & r);
+void sccSpeed(Reddit & r); // declares SCC racer
 void sccSpeed(Reddit & r)
 {
+  auto t1 = std::chrono::high_resolution_clock::now(); // Tracks starting time
+  r.SCCs(); // Calls kosaraju's SCC
+  auto t2 = std::chrono::high_resolution_clock::now(); // Tracks Kosaraju time
+  r.StronglyCC(); // Calls tarjan's SCC
+  auto t3 = std::chrono::high_resolution_clock::now(); // Tracks end time
 
-  auto t1 = std::chrono::high_resolution_clock::now();
-  r.SCCs();
-  auto t2 = std::chrono::high_resolution_clock::now();
-  r.StronglyCC();
-  auto t3 = std::chrono::high_resolution_clock::now();
+  auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count(); // Calculates Kosaraju time
+  auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>( t3 - t2 ).count(); // Calculates Tarjan time
 
-  auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-  auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>( t3 - t2 ).count();
-
-  std::cout << "Kosaraju's Algo: " << duration1 << std::endl; 
-  std::cout << "Tarjan's Algo: " << duration2 << std::endl;
+  std::cout << "Kosaraju's Algo: " << duration1 << std::endl; // Prints time take for Kosaraju in milliseconds
+  std::cout << "Tarjan's Algo: " << duration2 << std::endl; // Prints time take for Tarjan in milliseconds
 }
 
-int main() {
-  
-  // Reddit red("tests/small.csv");
-  // Reddit red("tests/medium.csv");
-  // Reddit red("tests/large2.csv");
-  Reddit red("data/connected.csv");
-  // sccSpeed(red);
-
-  // Graph copy = red.getTranspose();
-  // red.StronglyCC();
-
-  // auto t1 = std::chrono::high_resolution_clock::now();
-  // red.TraversalDFS();
-  // auto t2 = std::chrono::high_resolution_clock::now();
-  
-
-  // auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-  // std::cout << "DFS runtime: " << duration1 << std::endl; 
-
-  // std::cout << "Graph Constructed!" << std::endl; 
-  // red.printPath(red.BFS("rarepuppers", "erieco"));
-  // red.printPath(red.IDS("rarepuppers", "erieco", 4));
-
-  // std::cout << red.getSentiment(red.BFS("1", "3")) << std::endl;
-  // std::cout << red.getSentiment(red.BFS("1", "2")) << std::endl;
-  // std::cout << red.getSentiment(red.BFS("1", "4")) << std::endl;
-  // std::cout << red.getSentiment(red.BFS("1", "5")) << std::endl;
-  // std::cout << red.getSentiment(red.BFS("3", "1")) << std::endl;
-  // std::cout << red.getSentiment(red.BFS("3", "3")) << std::endl;
-
-
-  // vector<Vertex> expected = red.getGraph().getVertices();
-  // vector<Vertex> actual = red.TraversalDFS();
-  
-  // sort(expected.begin(), expected.end());
-  // sort(actual.begin(), actual.end());
-  // if(expected == actual) std::cout << "Gottem" << std::endl; 
-
-  std::cout << "BFS: " << std::endl; 
-  vector<Vertex> path1 = red.BFS("518circlejerk", "ironscape");
-  red.printPath(path1);
-  std::cout << "Path sentiment: " << red.getSentiment(path1) << std::endl;
-
-  std::cout << "\nIDS: " << std::endl; 
-  vector<Vertex> path2 = red.IDS("518circlejerk", "ironscape", 3);
-  red.printPath(path2);
-  std::cout << "Path sentiment: " << red.getSentiment(path2) << std::endl;
-
-
-  // vector<vector<Vertex>> sccs = red.SCCs();  
-  // int count = 0;
-  // int total = 0;
-  // for(size_t x = 0; x < sccs.size(); x++)
-  // {
-  //   size_t sz = sccs[x].size();
-  //   //if(sz > 2) std::cout << sz << std::endl;
-  //   total += sz;
-  //   if(sz < 2) count++;
-  // }
-  // std::cout << red.getGraph().getVertices().size() << std::endl;
-  // std::cout << total << std::endl;
-  // std::cout << count << std::endl;
-
-  // int bfs = 0;
-  // int ids = 0;
-  // for(int i = 0; i < 100; i++)
-  // {
-  //   if((i+1)%10 == 0) std::cout << i+1 << std::endl;
-  //   auto t1 = std::chrono::high_resolution_clock::now();
-  //   red.BFS("rarepuppers", "erieco");
-  //   auto t2 = std::chrono::high_resolution_clock::now();
-  //   red.IDS("rarepuppers", "erieco", 5);
-  //   auto t3 = std::chrono::high_resolution_clock::now();
-  //   bfs += std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-  //   ids += std::chrono::duration_cast<std::chrono::microseconds>( t3 - t2 ).count();
-  // }
-  // std::cout << "Average time for BFS: " << bfs/100 << std::endl;
-  // std::cout << "Average time for IDS: " << ids/100 << std::endl;
- 
-
-  // printVec(red.getGraph().getAdjacent("simplerockets"));
-  
-  
+int main() 
+{
+  // Sample function calls:
+  Reddit red("data/connected.csv"); // Creates reddit object
+  // WARNING: Due to our large dataset, Both SCC implementations will run into Stack Overflow
+  // Set stack size to unlimited using "ulimit -s unlimited" before calling DFSTraversal(), SCCs(), or StronglyCC()
+  vector<vector<Vertex>> kosaraju = red.SCCs(); // Kosarajus Strongly Connected Componets Implementation
+  vector<vector<Vertex>> tarjan = red.StronglyCC(); // Kosarajus Strongly Connected Componets Implementation
+  vector<Vertex> fulltraversal = red.TraversalDFS(); // Full graph DFS Traversal
+  vector<Vertex> bfspath = red.BFS("rarepuppers", "erieco"); // Get BFS path
+  vector<Vertex> idspath = red.IDS("rarepuppers", "erieco", 4); // Get IDS path
+  red.printPath(bfspath); // Print BFS path
+  red.printPath(idspath); // Print IDS path
+  std::cout << "Path sentiment (BFS): " << red.getSentiment(bfspath) << std::endl; // Print BFS path sentiment
+  std::cout << "Path sentiment (IDS): " << red.getSentiment(idspath) << std::endl; // Print IDS path sentiment
   return 0;
 }
 
-//reference:
-// Average time: 113868
-//copy:
-//Average time: 397093
+// Some cool observations: 
 
 // *****************************Search Time & Memory Test*******************************
 // Tests for search between "rarepuppers" and "erieco"
@@ -122,9 +46,6 @@ int main() {
 // Average time for IDS: 118.075 ms
 // Memeory Allocated BFS:  6.191528 MB 
 // Memeory Allocated IDS: 14.629688 MB
-
-
-
 
 // *****************************Search Time & Memory Test*******************************
 // Tests between Kosaraju vs Tarjan
